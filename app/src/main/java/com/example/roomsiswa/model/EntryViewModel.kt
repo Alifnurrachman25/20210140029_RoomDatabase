87cadb8055b6e6ca26abd9b582d01d3f6b1a5a9d
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-12-04 15:59:37 +0700

    Membuat class EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
 
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    /*
+    *Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
