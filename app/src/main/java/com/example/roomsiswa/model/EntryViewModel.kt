ccdf0cb7a83356a1285c25487a38ea9ed93a3890
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-12-04 16:11:46 +0700

    Menambahkan isi class EntryViewModeldengan menambahkan method validasiinput,updateuistate,savesiswa.

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
@@ -12,15 +13,42 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
      */
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa(){
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
 )
 
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telepon: String = "",
+    val telpon: String = "",
+)
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
